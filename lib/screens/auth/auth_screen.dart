d37d41052ad361a7169371dbb5f7d7b0a509d41e
CaoThang <thangly2k1@gmail.com>
2022-05-30 14:25:24 +0700

    set up folders

---
+++ lib/screens/auth/auth_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class AuthScreen extends StatefulWidget {
+  static const String routeName = '/auth-screen';
+  const AuthScreen({ Key? key }) : super(key: key);
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
