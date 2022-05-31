0ed3e3e83d4e9d66ef2f04fde7362373d9b9fbd6
CaoThang <thangly2k1@gmail.com>
2022-05-31 16:10:11 +0700

    add case homescreen and bottombar

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,4 +1,6 @@
 import 'package:amazon_clone/screens/auth/auth_screen.dart';
+import 'package:amazon_clone/screens/home/home_screen.dart';
+import 'package:amazon_clone/widgets/bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 Route<dynamic> generateRoute(RouteSettings routeSettings) {
@@ -7,6 +9,16 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
       return MaterialPageRoute(
         settings: routeSettings,
         builder: (_) => const AuthScreen(),
+      );
+    case HomeScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const HomeScreen(),
+      );
+    case BottomBar.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const BottomBar(),
       );
     default:
       return MaterialPageRoute(
