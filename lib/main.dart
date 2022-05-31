3bcdd38c00e9f8757bea646a246b562c5407d00c
CaoThang <thangly2k1@gmail.com>
2022-05-31 16:17:51 +0700

    add MultiProvider, setState getUserData, check token using provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,34 @@
 import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:amazon_clone/router/router.dart';
 import 'package:amazon_clone/screens/auth/auth_screen.dart';
+import 'package:amazon_clone/screens/auth/auth_service.dart';
+import 'package:amazon_clone/widgets/bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(
+      create: (context) => UserProvider(),
+    ),
+  ], child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthService authService = AuthService();
+  @override
+  void initState() {
+    super.initState();
+    authService.getUserData(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +49,12 @@ class MyApp extends StatelessWidget {
         useMaterial3: true, // can remove this line
       ),
       onGenerateRoute: (settings) => generateRoute(settings),
-      home: const AuthScreen(),
+      home: Provider.of<UserProvider>(context)
+              .user
+              .token
+              .isNotEmpty //check token != null
+          ? const BottomBar() // token is valid navigator BottomBar
+          : const AuthScreen(), // else navigator AuthenScreen
     );
   }
 }
