2721385a93ee5a68e116f599296d19a0e8f69de8
CaoThang <thangly2k1@gmail.com>
2022-06-02 13:19:33 +0700

    create function pickImages

--- lib/constants/utils.dart
+++ lib/constants/utils.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 
 void showSnackBar(BuildContext context, String text) {
   ScaffoldMessenger.of(context).showSnackBar(
@@ -8,4 +8,22 @@ void showSnackBar(BuildContext context, String text) {
       content: Text(text),
     ),
   );
+}
+
+Future<List<File>> pickImages() async {
+  List<File> images = [];
+  try {
+    var files = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: true,
+    );
+    if (files != null && files.files.isNotEmpty) {
+      for (int i = 0; i < files.files.length; i++) {
+        images.add(File(files.files[i].path!));
+      }
+    }
+  } catch (e) {
+    debugPrint(e.toString());
+  }
+  return images;
 }
